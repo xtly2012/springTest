24bd47f224b11fd32e9a8446b5270253e5a20fbb
chenfayong <chenfayong@qianmi.com>
2015-03-13 00:03:22 +0800

    aaaa

--- springTest/src/main/java/com/chen/activemq/ActivemqTest.java
+++ springTest/src/main/java/com/chen/activemq/ActivemqTest.java
@@ -22,7 +22,7 @@ public class ActivemqTest {
             e.printStackTrace();
         }
         StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
-        System.out.println("===================");
+        System.out.println("===================   ");
 	}
 	
 	public static void main(String[] argus) {		
